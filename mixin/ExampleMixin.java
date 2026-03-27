ac2ff70848311b2f2342c148c6190eaf38916d6a
SaphicDeveloper2 <sladesaphire@gmail.com>
2026-03-27 17:48:33 +0000

    Delete mixin directory

--- mixin/ExampleMixin.java
+++
@@ -1,6 +0,0 @@
-package com.sapphic.ssl.mixin;
-
-// This stub is intentionally empty.
-// The active Mixin for SSL is ChunkGeneratorMixin.
-// Mixin framework requires at least one class declared in the mixins.json package;
-// ChunkGeneratorMixin satisfies that requirement.
